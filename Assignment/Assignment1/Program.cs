80991a78c90ed39cced77ea5d4a1bfa3c76238cc
DESKTOP-V36UQ5Q\subhana_teksewa <subhana@teksewa.com>
2024-08-05 14:44:04 +0545

    Assignment 1

---
+++ Assignment/Assignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter the electric unit: ");
+int  unit = int.Parse(Console.ReadLine());
+char notify;
+//using if-else
+if (unit >=1 && unit <= 20){
+    Console.WriteLine("The total amount is Rs 100");
+}
+else if (unit >= 21 && unit <= 25){
+    Console.WriteLine($"The total amount is Rs: {100 + (unit -20) * 5}" );
+}
+else if(unit >= 26 && unit <= 50){
+    Console.WriteLine($"The total amount is Rs : { 125 + (unit - 25 ) * 10}" );
+}else{
+    Console.WriteLine("The total amount is Rs 2000");
+}
+//using if and switch
+if (unit >=1 && unit <= 20){
+    notify = 'a';
+}
+else if (unit >= 21 && unit <= 25){
+   notify = 'b';
+}
+else if(unit >= 26 && unit <= 50){
+    notify = 'c';
+}else{
+    notify = 'd';
+}
+switch (notify){
+    case 'a' : Console.WriteLine("The total amount is Rs 100"); break;
+    case 'b':  Console.WriteLine($"The total amount is Rs: {100 + (unit -20) * 5}" );   break;
+    case 'c' : Console.WriteLine($"The total amount is Rs : { 125 + (unit - 25 ) * 10}" ); break;
+    default :  Console.WriteLine("The total amount is Rs 2000"); break;
+}
