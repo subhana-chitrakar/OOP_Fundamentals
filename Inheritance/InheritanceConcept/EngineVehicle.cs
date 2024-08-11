f98e9d13df58843ce35ffc2280a2d78015b7f550
Subhana Chitrakar <subhana.chitrakar@gmail.com>
2024-08-11 16:33:44 +0545

    Changes to enginevehicle

--- Inheritance/InheritanceConcept/EngineVehicle.cs
+++ Inheritance/InheritanceConcept/EngineVehicle.cs
@@ -1,4 +1,5 @@
-public class EngineVehicle : Vehicle, IVehicle //multiple inheritance
+public class EngineVehicle : Vehicle, IVehicle //multiple inheritance is possible with interfaces. It inherits from Vehicles and implements from IVehicle
+
 {
 
      public EngineVehicle(string number,float enginecc, float mileage) : base(number)
@@ -11,6 +12,7 @@ public class EngineVehicle : Vehicle, IVehicle //multiple inheritance
 
     public float EngineCC { get; set; }
     public float Mileage { get; set; }
+    //we have to implement interface 
     public float GetDistanceCovered(float oilUsed) => Mileage * oilUsed;
 
 }
--- Inheritance/InheritanceConcept/IVehicle.cs
+++ Inheritance/InheritanceConcept/IVehicle.cs
@@ -1,4 +1,4 @@
-//Interface
+//interface : entity that wraps functionalities.The functionalities can be implemented by any class 
 public interface IVehicle
 {
     public float GetDistanceCovered(float consumption);
